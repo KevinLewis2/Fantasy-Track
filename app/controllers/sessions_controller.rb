29a4e8868dd8478c32d84418bb97af71e2a35588
kevinlewis2 <kevin-lewis@uiowa.edu>
2015-10-27 18:08:11 +0000

    Added sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
-  def user_params
-    params.require(:user).permit(:user_id, :email)
-  end
+  #def user_params
+    #params.require(:user).permit(:user_id, :email)
+  #end
   
   def new
    # default: render 'new' template
